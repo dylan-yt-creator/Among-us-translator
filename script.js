3fb91da40fcc907dd9c38aaf41d516be152ba02a
dylan-yt-creator <dylan.yt.creator@gmail.com>
2026-04-28 14:44:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const dictionary = {
+  "hello": "zarn",
+  "world": "mok",
+  "i": "ka",
+  "love": "zir",
+  "you": "tan"
+};
 
+function translateText() {
+  const input = document.getElementById("inputText").value;
+
+  const translated = input
+    .toLowerCase()
+    .split(" ")
+    .map(word => dictionary[word] || word)
+    .join(" ");
+
+  document.getElementById("outputText").innerText = translated;
+}
